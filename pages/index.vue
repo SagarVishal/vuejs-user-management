037d80f856b7b0d17d4c78958fb7c79daa31a825
Vishal <vishalsagar.v7@gmail.com>
2021-08-20 12:10:17 +0530

    Initial Setup

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-card
+    class="w-100 mb-4"
+    header-tag="header"
+    header-class="d-flex align-items-center justify-content-between"
+    body-class="custom-body p-0"
+    footer-tag="footer"
+    footer-class="custom-footer bt-1"
+    data-test="service-account"
+  >
+
+  <template align-h="center" #header>
+      <h5 class="mb-0">
+        <strong>Users</strong>
+      </h5>
+
+      <b-button
+        variant="primary"
+        >Create User</b-button
+      >
+    </template>
+
+  </b-card>
+</template>
+
+<script>
+export default {}
+</script>
